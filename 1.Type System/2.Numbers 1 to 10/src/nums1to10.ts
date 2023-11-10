24c1fa317e563b737a8aba04d9d8e7b75b9ee4bd
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2023-11-10 12:15:24 +0200

    Create nums1to10.ts

---
+++ 1.Type System/2.Numbers 1 to 10/src/nums1to10.ts
@@ -0,0 +1,12 @@
+function nums1to10() {
+    let result = "";
+    for (let i = 1; i <= 10; i++) {
+      result += i;
+      if (i < 10) {
+        result += "|";
+      }
+    }
+    console.log(result);
+}
+
+nums1to10();
