123e75116278339d95a9239bf0c09b471cfcf1be
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2023-11-10 12:19:40 +0200

    Create convert.js

---
+++ 1.Type System/4.ConvertorInchToSm/dist/convert.js
@@ -0,0 +1,11 @@
+"use strict";
+// function calc(num1: number, num2: number) {
+//     let area = num1 * num2;
+//     console.log(area);
+// }
+// calc(6, 8)
+function convert(num) {
+    let result = num * 2.54;
+    console.log(result);
+}
+convert(5);
