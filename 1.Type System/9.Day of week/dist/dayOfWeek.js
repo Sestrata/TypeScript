55b7425b1c71b5ff347f7a17118d0bbd9b434cd2
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2023-11-11 10:27:54 +0200

    Create dayOfWeek.js

---
+++ 1.Type System/9.Day of week/dist/dayOfWeek.js
@@ -0,0 +1,39 @@
+"use strict";
+function printDayNumber(dayOfWeek) {
+    switch (dayOfWeek) {
+        case "Monday":
+            console.log("1");
+            break;
+        case "Tuesday":
+            console.log("2");
+            break;
+        case "Wednesday":
+            console.log("3");
+            break;
+        case "Thursday":
+            console.log("4");
+            break;
+        case "Friday":
+            console.log("5");
+            break;
+        case "Saturday":
+            console.log("6");
+            break;
+        case "Sunday":
+            console.log("7");
+            break;
+        default:
+            console.log("Error");
+            break;
+    }
+}
+const dayOfWeek = "Invalid";
+printDayNumber(dayOfWeek);
+// enum DaysOfWorkWeek {
+//     Monday,
+//     Tuesday,
+//     Wednesday,
+//     Thursday,
+//     Friday
+// }
+// console.log("enum", DaysOfWorkWeek); 
