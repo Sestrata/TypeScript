67234a85a8ce25467282e015d5c307305c5ee52c
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2023-11-11 10:24:43 +0200

    Create largestNum.ts

---
+++ 1.Type System/7.Largest number/src/largestNum.ts
@@ -0,0 +1,5 @@
+function largestNum(num1: number, num2: number, num3: number) {
+    const largestNumber = Math.max(num1, num2, num3);
+    console.log(`The largest number is ${largestNumber}.`);
+}
+largestNum(-3, -5, -22.5)
