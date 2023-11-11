6d44e00005ffcefe638c99e2f8946efb822bc6c5
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2023-11-11 10:24:10 +0200

    Create largestNum.js

---
+++ 1.Type System/7.Largest number/dist/largestNum.js
@@ -0,0 +1,6 @@
+"use strict";
+function largestNum(num1, num2, num3) {
+    const largestNumber = Math.max(num1, num2, num3);
+    console.log(`The largest number is ${largestNumber}.`);
+}
+largestNum(-3, -5, -22.5);
