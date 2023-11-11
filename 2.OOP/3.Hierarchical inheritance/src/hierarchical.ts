6dcbe98f9db32edd05990ef21fa44fb8b2a42b39
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2023-11-11 10:34:58 +0200

    Create hierarchical.ts

---
+++ 2.OOP/3.Hierarchical inheritance/src/hierarchical.ts
@@ -0,0 +1,25 @@
+class Animal {
+    public eat() {
+      console.log("eating...");
+    }
+  }
+  
+  class Dog extends Animal {
+    public bark() {
+      console.log("barking...");
+    }
+  }
+  
+  class Cat extends Animal {
+    public meow() {
+      console.log("meowing...");
+    }
+  }
+  
+  const dog = new Dog();
+  dog.eat();
+  dog.bark();
+  
+  const cat = new Cat();
+  cat.eat();
+  cat.meow();
