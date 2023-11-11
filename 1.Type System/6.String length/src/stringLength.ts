533288a472e8d936db1013138f201da7c22c0a30
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2023-11-11 10:22:57 +0200

    Create stringLength.ts

---
+++ 1.Type System/6.String length/src/stringLength.ts
@@ -0,0 +1,10 @@
+
+function strLength(str: string) {
+    let result = 0;
+    for (let i = 1; i <= str.length; i++) {
+        result++;
+    }
+    console.log(result);
+}
+
+strLength("chocolate")
