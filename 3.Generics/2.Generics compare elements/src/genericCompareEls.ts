2adfbf518e1b2757ceb7510e4652f592021c2d89
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2023-11-12 11:35:29 +0200

    Create genericCompareEls.ts

---
+++ 3.Generics/2.Generics compare elements/src/genericCompareEls.ts
@@ -0,0 +1,23 @@
+class CompareElements<T> {
+    private elements: T[];
+
+    constructor(elements: T[]) {
+        this.elements = elements;
+    }
+
+    compare(comparator: T): number {
+        let count = 0;
+        for (const element of this.elements) {
+            if (element === comparator) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+let c1 = new CompareElements(['a', 'b', 'ab', 'abc', 'cba', 'b']);
+console.log(c1.compare('b'));
+
+let c2 = new CompareElements([1, 2, 3, 4, 5, 1, 1]);
+console.log(c2.compare(1));
